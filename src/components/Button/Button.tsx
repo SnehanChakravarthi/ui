5d338028e04e349dc27d04ea6afbe20c03639b89
Snehan Chakra Varthi <snehan@motsats.com>
2023-10-09 01:44:29 +0200

    Fix: Button props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,17 @@
-type Props = {
-  text: string;
-};
+import React from 'react';
 
-const Button: React.FC<Props> = ({ text }) => {
-  return <div>{text}</div>;
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
+}
+
+const Button: React.FC<ButtonProps> = ({ variant, ...props }) => {
+  const colorClass = variant === 'primary' ? 'blue' : 'red';
+  return (
+    <button style={{ backgroundColor: colorClass }} {...props}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
